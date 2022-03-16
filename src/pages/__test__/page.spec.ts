2ca3ab651bdc3d4959d82203ae270bbae8df3016
psi <cy.lonsido@gmail.com>
2022-03-16 22:12:40 +0800

    update test sample

--- src/pages/__test__/page.spec.ts
+++ src/pages/__test__/page.spec.ts
@@ -1,12 +1,28 @@
+import IndexPage from '../IndexPage.vue'
+import { mount } from '@vue/test-utils'
+import { mergeWith } from 'lodash'
+
+
+const createWrapper = (overrides?: any) => {
+  const defaultMountingOptions = {
+    //
+  }
+
+  return mount(
+    IndexPage,
+    mergeWith(defaultMountingOptions, overrides)
+  )
+}
+
 describe(
   'Sample Test',
   () => {
     test(
       'sanity check',
       () => {
-        const gg = 'gg'
+        const wrapper = createWrapper()
 
-        expect(gg).toEqual('gg')
+        console.log(wrapper)
       }
     )
   }
