908fc8c4be6f85606508771a93ad249b544275a4
psi <cy.lonsido@gmail.com>
2022-03-19 13:32:47 +0800

    define env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,4 +6,13 @@ declare namespace NodeJS {
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined
     VUE_ROUTER_BASE: string | undefined
   }
+
+  interface ProcessEnv {
+    SERVER_URL: string
+    SERVER_BASEURL: string
+    SERVER_PORT: string
+    SERVER_PROTOCOL: 'http' | 'https'
+    DEV_PORT: string
+  }
 }
+
