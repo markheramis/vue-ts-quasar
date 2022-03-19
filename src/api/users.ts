557f8eb54de9655eb1ff8086654b620e0d5280c3
psi <cy.lonsido@gmail.com>
2022-03-19 13:35:11 +0800

    define user api via axios

---
+++ src/api/users.ts
@@ -0,0 +1,20 @@
+import { api as request } from 'boot/axios'
+
+
+type loginParams = {
+  username: string
+  password: string
+}
+
+export const login = (params: loginParams) =>
+  request({
+    url: '/auth/login',
+    method: 'post',
+    data: params
+  })
+
+export const getUserInfo = () =>
+  request({
+    url: '/auth/me',
+    method: 'get'
+  })
