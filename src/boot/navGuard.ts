ee1945f1f397d74d771d53da914c336779d5ea44
psi <cy.lonsido@gmail.com>
2022-03-22 06:45:52 +0800

    update navguard

--- src/boot/navGuard.ts
+++ src/boot/navGuard.ts
@@ -3,6 +3,7 @@ import NProgress from 'nprogress'
 import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
 import useUserStore from '@/stores/user'
 import { getToken } from '@/utils/storage'
+import usePermissionStore from '@/stores/permission'
 
 /* Turn off loading spinner
  *
@@ -16,73 +17,91 @@ NProgress.configure({ showSpinner: false })
 
 const whiteList = ['/login', 'auth-redirect']
 
-/**
- * Callback function to pass on beforeGuard hook.
- */
-
-const beforeGuard = async (
-  to: RouteLocationNormalized,
-  _: RouteLocationNormalized,
-  next: NavigationGuardNext
-) => {
-  /**
-   * Instantiate user store
-   */
-
-  const store = useUserStore()
-
-  /**
-   * Retrieve token if present from device storage
-   */
-
-  store.token = getToken() as string
-
-  // Start the progress bar
-  NProgress.start()
-
-  if (store.token) {
-    if (to.path === '/login') {
-      // If is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
-    } else {
-      // Check whether the user has obtained his permission roles
-      if (store.roles.length === 0) {
-        try {
-          await store.GetUserInfo()
-
-          // Hack: ensure addRoutes is complete
-          // Set the replace: true, so the navigation will not leave a history record
-          next({ ...to, replace: true })
-        } catch (err) {
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
-      } else {
-        next()
-      }
-    }
-  } else {
-    // Has no token
-    if (whiteList.indexOf(to.path) !== -1) {
-      // In the free login whitelist, go directly
-      next()
-    } else {
-      // Other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
-    }
-  }
-}
-
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ router }) => {
+
+  /**
+   * Callback function to pass on beforeGuard hook.
+   */
+
+  const beforeGuard = async (
+    to: RouteLocationNormalized,
+    _: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+
+    /**
+     * Instantiate user store
+     */
+
+    const userStore = useUserStore()
+
+    /**
+     * Instantiate permission store
+     */
+
+    const permissionStore = usePermissionStore()
+
+    /**
+     * Retrieve token if present from device storage
+     */
+
+    userStore.token = getToken() as string
+
+    // Start the progress bar
+    NProgress.start()
+
+    if (userStore.token) {
+      if (to.path === '/login') {
+        // If is logged in, redirect to the home page
+        next({ path: '/' })
+        NProgress.done()
+      } else {
+        // Check whether the user has obtained his permission roles
+        if (userStore.roles.length === 0) {
+          try {
+            await userStore.GetUserInfo()
+            const roles = userStore.roles
+
+            permissionStore.GenerateRoutes(roles)
+
+            permissionStore.dynamicRoutes.forEach(route => {
+              router.addRoute(route)
+            })
+
+            // Hack: ensure addRoutes is complete
+            // Set the replace: true, so the navigation will not leave a history record
+            next({ ...to, replace: true })
+          } catch (err) {
+            next(`/login?redirect=${to.path}`)
+            NProgress.done()
+          }
+        } else {
+          next()
+        }
+      }
+    } else {
+      // Has no token
+      if (whiteList.indexOf(to.path) !== -1) {
+        // In the free login whitelist, go directly
+        next()
+      } else {
+        // Other pages that do not have permission to access are redirected to the login page.
+        next(`/login?redirect=${to.path}`)
+        NProgress.done()
+      }
+    }
+  }
+
+
+
   /**
    * Trigger global hook before the actual navigation
    */
 
   router.beforeEach(beforeGuard)
+ 
 
   /**
    * Set nprogress to done when route is resolved. Also
@@ -95,10 +114,8 @@ export default boot(async ({ router }) => {
     NProgress.done()
 
     // set page title
-    /*
-    const title = settings.title
+    const title = 'App'
     if (to.name) document.title = `${to.name?.toString()} - ${title}`
     else document.title = title
-    */
   })
 })
