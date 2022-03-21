9428a7dea20aa47a7c6fb7bf1e84e8627c149b6f
psi <cy.lonsido@gmail.com>
2022-03-21 15:27:39 +0800

    update route, auto-imports

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,12 +5,17 @@ const constantRoutes: RouteRecordRaw[] = [
   {
     path: '/login',
     component: () => import('pages/auth/login/LoginIndex.vue'),
+    name: 'Login'
   },
 
   {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
+    name: 'Dashboard',
+    meta: {
+      icon: 'home',
+    },
     children: [
       {
         path: 'dashboard',
@@ -18,12 +23,73 @@ const constantRoutes: RouteRecordRaw[] = [
         name: 'Dashboard',
         meta: {
           title: 'dashboard',
-          icon: 'dashboard',
+          icon: 'home',
           affix: true,
         },
       },
     ],
   },
+
+  {
+    path: '/user',
+    component: Layout,
+    redirect: '/user/index',
+    name: 'User',
+    meta: {
+      icon: 'person',
+    },
+    children: [
+      {
+        path: 'index',
+        component: () => import('pages/user/UserIndex.vue'),
+        name: 'User Index',
+        meta: {
+          title: 'User Index',
+          icon: 'people',
+        },
+      },
+      {
+        path: 'create',
+        component: () => import('pages/user/UserCreate.vue'),
+        name: 'User Create',
+        meta: {
+          title: 'User Create',
+          icon: 'group_add',
+        },
+      },
+    ],
+  },
+
+    {
+    path: '/user2',
+    component: Layout,
+    redirect: '/user/index',
+    name: 'User 2',
+    meta: {
+      icon: 'group',
+    },
+    children: [
+      {
+        path: 'index',
+        component: () => import('pages/user/UserIndex.vue'),
+        name: 'User Index 2',
+        meta: {
+          title: 'User Index 2',
+          icon: 'people',
+        },
+      },
+      {
+        path: 'create',
+        component: () => import('pages/user/UserCreate.vue'),
+        name: 'User Create 2',
+        meta: {
+          title: 'User Create 2',
+          icon: 'group_add',
+        },
+      },
+    ],
+  },
+
 ]
 
 export const asyncRoutes: RouteRecordRaw[] = [
