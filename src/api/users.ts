7b6134410398704bfd7054cca527fa16b08ed6cf
psi <cy.lonsido@gmail.com>
2022-03-25 12:31:40 +0800

    add user request api's

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,11 +1,15 @@
 import { api as request } from 'boot/axios'
 
-type loginParams = {
+type LoginParams = {
   username: string
   password: string
 }
 
-export const login = (params: loginParams) =>
+type ValidateCodeParams = {
+  code: string
+}
+
+export const login = (params: LoginParams) =>
   request({
     url: '/auth/login',
     method: 'post',
@@ -18,13 +22,45 @@ export const getUserInfo = () =>
     method: 'get',
   })
 
-type validateCodeParams = {
-  code: string
-}
-export const validateCode = (params: validateCodeParams) =>
+export const validateCode = (params: ValidateCodeParams) =>
   request({
     url: '/auth/mfa/verify',
     method: 'post',
     data: params,
   })
 
+/* eslint-disable-next-line */
+export const getUsers = (params: any) =>
+  request({
+    url: '/user',
+    method: 'get',
+    params,
+  })
+
+export const getUserById = (id: string) =>
+  request({
+    url: `/user/${id}`,
+    method: 'get',
+  })
+
+/* eslint-disable-next-line */
+export const createUser = (params: any) =>
+  request({
+    url: '/user',
+    method: 'post',
+    data: params,
+  })
+
+/* eslint-disable-next-line */
+export const updateUser = (id: number, params: any) =>
+  request({
+    url: `/user/${id}`,
+    method: 'put',
+    data: params,
+  })
+
+export const deleteUser = (id: number) =>
+  request({
+    url: `/user/${id}`,
+    method: 'delete',
+  })
