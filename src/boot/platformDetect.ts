e96866640e264c0b2f90f826f4ea27bfaf430e88
psi <cy.lonsido@gmail.com>
2022-03-31 07:09:02 +0800

    update nprogress styles

--- src/boot/platformDetect.ts
+++ src/boot/platformDetect.ts
@@ -3,6 +3,12 @@ import { Platform } from 'quasar'
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(({ /* */ }) => {
-  console.log(Platform.is)
-})
+export default boot(
+  (
+    {
+      /* */
+    }
+  ) => {
+    console.log(Platform.is)
+  }
+)
